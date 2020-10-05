64ecda0cd356ffa119c3fa7d65f7754fc2e9051e
David Case <david@kronoverse.io>
2020-10-05 11:47:46 -0400

    update spends route

--- server.js
+++ server.js
@@ -97,9 +97,9 @@ app.get('/utxos/:script', async (req, res, next) => {
     }
 });
 
-app.get('/spends/:txid/:vout', async (req, res, next) => {
+app.get('/spends/:loc', async (req, res, next) => {
     try {
-        const {txid, vout} = req.params;
+        const {txid, vout} = req.params.loc.split('_o');
         res.send(await blockchain.spends(txid, parseInt(vout, 10)));
     } catch (e) {
         next(e);
